fdf624870197adace36cb65dd8d265e9b9556202
logonsamba <logonsamba@hotmail.com>
2019-11-03 13:43:55 +0530

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp8Features
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        Person(string firstName, string middleName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.MiddleName = middleName;
+            this.LastName = lastName;
+        }
+        public State PatternMatching(State state, Transition transition)
+        {
+            return state=(state,transition)switch
+            {
+                (state.Running, Transition.Suspended) => State.Suspended,
+                (state.Suspended, Transition.Resume) => State.Running,
+                (state.Suspended, Transition.Terminate) => State.NotRunning,
+                (state.NotRunning, Transition.Activate) => State.Running,
+                _ => throw new InvalidOperationException()
+            }
+           
+        }
+    }
+    public enum State
+    {
+        Running,
+        Suspended,
+        NotRunning
+    }
+    public enum Transition
+    {
+        Running,
+        Suspended,
+        NotRunning
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Csharp8Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+   
+
+}
